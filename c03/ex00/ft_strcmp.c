f222e91d15b7179e2cc26975b8acc4b360e55a2c
LotuS16 <reynaldosetyanto@gmail.com>
2023-09-28 17:42:20 +0700

    update in ex00

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -7,20 +7,21 @@ int ft_strcmp(char *s1, char *s2)
     int i;
     i = 0;
 
-    while (s1[i] != '\0' && s2[i] != '\0')
+    while (s1[i] != '\0' || s2[i] != '\0')
     {
-        if(s1[i] == s2[i])
+        if(s1[i] != s2[i])
         {
-            i++;
+            return (s1[i]-s2[i]);
         }
+        i++;
     }
-    return (s1[i]-s2[i]);
+    return (0);
 }
 
 int main()
 {
     char s1[] = "allo";
-    char s2[] = "alo";
+    char s2[] = "all";
     int res = ft_strcmp(s1, s2);
 
     printf("test : %d\n", res);
